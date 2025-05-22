f2d3af28aee9981b5a80dc184624391c47c7074e
Lala <laleimamhuseynova@gmail.com>
2025-05-22 06:40:04 +0400

    Update: Added new features or changes

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from .models import PostModel
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','description','date']
+    list_display_links=['title']
+    readonly_fields = ['date']
+    #list_editable = ['description']
+    search_fields = ['title']
+    list_filter = ['date','title']
+    fieldsets = (
+      (
+         'Main part',{
+           'fields':('title',)
+      }
+
+    ),
+       (
+          'Second part', {
+          'fields': ('description','date')
+      }
+
+      )
+    )
+
 # Register your models here.
+admin.site.register(PostModel,PostAdmin)
+
+
--- post/views.py
+++ post/views.py
@@ -1,5 +1,16 @@
+
+from datetime import datetime
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect,reverse
+from django.views import View
+
+from .models import PostModel
+
+
+#function based views
+#class based views
+
 
 # Create your views here.
 def post(request):
@@ -7,4 +18,47 @@ def post(request):
 def detail(request):
     return HttpResponse("This is detail of previous article")
 def comments(request):
-    return HttpResponse("This is comments of out post")
+    if request.method=='GET':
+        return HttpResponse("This is GET method of showing comments.")
+    if request.method=="POST":
+        return HttpResponse("This is POST method of showing comments.")
+
+class Post(View):
+     def get(self,request):
+
+         #return HttpResponse("Class based views")
+        #şablon
+        # articles = [
+             # {
+             #   'title':'Earthquake happened in Turkiye',
+             #   'description':'People out of home',
+             #   'date': datetime.now()
+             # },
+             # {
+             #    'title': 'Our diplomats signed contract in China',
+             #    'description': 'Business will develop according to these contacts',
+             #    'date': datetime.now()
+             # }
+         #]
+         articles=PostModel.objects.order_by('-id').all()
+         return render(request,'articles.html',{
+             'posts': articles
+         })
+
+     #def post(self,request):
+      #   return HttpResponse("Post class based views")
+class CreatePost(View):
+    def get(self,request):
+        return render(request,'create_article.html')#url e bind etmeliyik
+
+    def post(self,request):
+        title=request.POST.get("title",'unknown')
+        description=request.POST.get("description",'unknown')
+        date=datetime.now()
+        PostModel.objects.create(
+            title=title,
+            description=description,
+            date=date
+
+        )
+        return redirect(reverse('list_articles'))
